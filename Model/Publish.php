ebb66141ef8aed595bdfbb1ea386d083cab484d0
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 13:24:38 -0700

    Magento Fork initial commit

--- Model/Publish.php
+++ Model/Publish.php
@@ -7,11 +7,7 @@ declare(strict_types=1);
 
 namespace Magento\MediaContentSynchronization\Model;
 
-use Magento\AsynchronousOperations\Api\Data\OperationInterfaceFactory;
-use Magento\Framework\Bulk\OperationInterface;
-use Magento\Framework\DataObject\IdentityGeneratorInterface;
 use Magento\Framework\MessageQueue\PublisherInterface;
-use Magento\Framework\Serialize\SerializerInterface;
 
 /**
  * Publish media content synchronization queue.
@@ -24,63 +20,26 @@ class Publish
     private const TOPIC_MEDIA_CONTENT_SYNCHRONIZATION = 'media.content.synchronization';
 
     /**
-     * @var OperationInterfaceFactory
-     */
-    private $operationFactory;
-
-    /**
-     * @var IdentityGeneratorInterface
-     */
-    private $identityService;
-
-    /**
      * @var PublisherInterface
      */
     private $publisher;
 
     /**
-     * @var SerializerInterface
-     */
-    private $serializer;
-
-    /**
-     * @param OperationInterfaceFactory $operationFactory
-     * @param IdentityGeneratorInterface $identityService
      * @param PublisherInterface $publisher
-     * @param SerializerInterface $serializer
      */
-    public function __construct(
-        OperationInterfaceFactory $operationFactory,
-        IdentityGeneratorInterface $identityService,
-        PublisherInterface $publisher,
-        SerializerInterface $serializer
-    ) {
-        $this->operationFactory = $operationFactory;
-        $this->identityService = $identityService;
-        $this->serializer = $serializer;
+    public function __construct(PublisherInterface $publisher)
+    {
         $this->publisher = $publisher;
     }
 
     /**
-     * Publish media content synchronization message to the message queue
-     *
-     * @param array $contentIdentities
+     * Publish media content synchronization message to the message queue.
      */
-    public function execute(array $contentIdentities = []) : void
+    public function execute() : void
     {
-        $data = [
-            'data' => [
-                'bulk_uuid' => $this->identityService->generateId(),
-                'topic_name' => self::TOPIC_MEDIA_CONTENT_SYNCHRONIZATION,
-                'serialized_data' => $this->serializer->serialize($contentIdentities),
-                'status' => OperationInterface::STATUS_TYPE_OPEN,
-            ]
-        ];
-        $operation = $this->operationFactory->create($data);
-
         $this->publisher->publish(
             self::TOPIC_MEDIA_CONTENT_SYNCHRONIZATION,
-            $operation
+            [self::TOPIC_MEDIA_CONTENT_SYNCHRONIZATION]
         );
     }
 }
